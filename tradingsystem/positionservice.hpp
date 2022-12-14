14df2e6030421cd51f1acaf2ba5eefeb32bc8ddb
zhuxingyu88 <zhuxingyu2000@foxmail.com>
2022-12-13 19:25:41 -0500

    Finished Bond Position Service

--- tradingsystem/positionservice.hpp
+++ tradingsystem/positionservice.hpp
@@ -36,6 +36,8 @@ public:
   // Get the aggregate position
   long GetAggregatePosition();
 
+  void ChangePosition(long quantity, string& book);
+
 private:
   T product;
   map<string,long> positions;
@@ -82,5 +84,76 @@ long Position<T>::GetAggregatePosition()
   // No-op implementation - should be filled out for implementations
   return 0;
 }
+
+template<typename T>
+void Position<T>::ChangePosition(long quantity, string &book) {
+    positions[book] += quantity;
+}
+
+class BondPositionService: public PositionService<Bond> {
+private:
+    map<string, Position<Bond> > bondPositions; // key: BookID, value: Position
+    vector<ServiceListener<Position<Bond> >* > bondPositionListeners;
+public:
+    Position<Bond>& GetData(string key) override {
+        return bondPositions.find(key)->second;
+    }
+
+    void OnMessage(Position<Bond> &data) override {}
+
+    void AddListener(ServiceListener<Position<Bond> > *listener) override {
+        bondPositionListeners.push_back(listener);
+    }
+
+    const vector< ServiceListener<Position<Bond> >* >& GetListeners() const override {
+        return bondPositionListeners;
+    }
+
+    void AddTrade(const Trade<Bond> &trade) override {
+        string bondID = trade.GetProduct().GetProductId();
+        string bookID = trade.GetBook();
+        long quantity = trade.GetQuantity();
+        Side side = trade.GetSide();
+        if(side==SELL)
+            quantity = -quantity;
+        auto tmp = bondPositions.find(bondID);
+        if (tmp == bondPositions.end()) {
+            Position<Bond> pos(trade.GetProduct());
+            pos.ChangePosition(quantity, bookID);
+            bondPositions.insert(make_pair(bondID, pos));
+            for(auto & bondPositionListener : bondPositionListeners)
+                bondPositionListener->ProcessAdd(pos);
+        } else {
+            tmp->second.ChangePosition(quantity, bookID);
+            for(auto & bondPositionListener : bondPositionListeners)
+                bondPositionListener->ProcessUpdate(tmp->second);
+        }
+    }
+};
+
+class BondTradeListener: public ServiceListener<Trade<Bond> > {
+private:
+    BondPositionService& bondPositionService;
+public:
+
+    BondTradeListener(BondPositionService& service): bondPositionService(service){}
+
+    void ProcessAdd(Trade<Bond> &data) override{
+        bondPositionService.AddTrade(data);
+    }
+
+    void ProcessRemove(Trade<Bond> &data) override {
+        Side side = data.GetSide();
+        if(side==BUY)
+            side = SELL;
+        else
+            side = BUY;
+        Trade<Bond> reverseTrade(data.GetProduct(),data.GetTradeId(),data.GetPrice(), data.GetBook(), data.GetQuantity(), side);
+        bondPositionService.AddTrade(reverseTrade);
+    }
+
+    void ProcessUpdate(Trade<Bond> &data) override{
+    }
+};
 
 #endif
